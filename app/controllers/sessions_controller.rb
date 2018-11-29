b8e2b20e75a19b52988906f1833de4d4c935cdc6
Maykell Sánchez Romero <maykell.sanchez@ministeriodelinterior.gob.ec>
2018-11-29 16:11:12 -0500

    Remember me functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,19 +7,21 @@ class SessionsController < ApplicationController
     @session = Session.new(session_params)
 
     user = User.find_by(email: @session.email.downcase)
+
     if user && user.authenticate(@session.password)
       log_in user
+      (@session.remember_me == '1') ? remember(user) 
+                                    : forget(user)
       redirect_to user 
     else
-      @session.email = ''
-      @session.password = ''      
+      @session.clear
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end  
   end
 
   def destroy
-    log_out	  
+    log_out if logged_in? 
     redirect_to root_path
   end
 
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,11 +1,16 @@
 class Session
   include ActiveModel::Model
 
-  attr_accessor :email, :password
+  attr_accessor :email, :password, :remember_me
 
   #validates :email, presence: true, length: {maximum: 255},
   #          format: {with: User::VALID_EMAIL_REGEX},
   #          uniqueness: { case_sensitive: false}
   #validates :password, presence: true, length {minimum: 6}
 
+  def clear
+    self.email = nil
+    self.password = nil
+    self.remember_me = nil
+  end
 end
